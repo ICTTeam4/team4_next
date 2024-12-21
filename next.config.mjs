722e56613fb8bdf7caf8015a0bfe48465e2eb075
hayoon <dbs9830@gmail.com>
2024-12-21 20:24:09 +0900

    websocket just only text, not db,photo,login,etc...i cant write korean i dont know why

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode:true,
-  // swcMinify:true,
+  // swcMinify:true,     프론트에서 경로를 요청하면, server로 값이 가게 만드는 파일이라 보면됨.
   async rewrites(){
       return[
-          
+        // ,{
+        //   // source: "/guestbook/:path*",
+        //   // destination: "http://localhost:8080/api/guestbook/:path*",
+        // },
       ];
   }
 };
